b2615a72dcab137c54aec8fda084c65dacd6f635
unknown <you@example.com>
2019-11-27 09:48:53 +0100

    added files and changes

--- app.js
+++ app.js
@@ -11,8 +11,10 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://Jonas:LocalLibrary@cluster0-mzd3j.azure.mongodb.net/ScoutsEnGidsenLede?retryWrites=true&w=majority';
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+// Set up mongoose connection
+var dev_db_url = 'mongodb+srv://Jonas:LocalLibrary@cluster0-mzd3j.azure.mongodb.net/ScoutsEnGidsenLede?retryWrites=true&w=majority'
+'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
